24134cea171c15b46249a641747ef7c9091f5063
mohamedahmed19922025-dev <mohamed.ahmed.19922025@gmail.com>
2026-01-22 20:57:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ st.title("hello")
 df = pd.read_excel("55.xlsx")
 event = st.dataframe(
     df,
-    column_config=column_configuration,
     use_container_width=True,
     hide_index=True,
     on_select="rerun",
     selection_mode="multi-row",
 )
                    
+
 
 
